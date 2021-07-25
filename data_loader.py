65f39d74d36151b5e1cb5c0ba8f68a5e1b34a527
Andy <bht26@cam.ac.uk>
2021-07-26 00:42:43 +0800

    refactor

--- data_loader.py
+++ data_loader.py
@@ -6,7 +6,7 @@ import torch
 from utils.util_dst import iter_dst_file, dict2list
 from utils.check_turn_info import decide_turn_domain
 
-# test git
+# test git2
 
 class DataLoader():
 	def __init__(self, config, load_src=False):
