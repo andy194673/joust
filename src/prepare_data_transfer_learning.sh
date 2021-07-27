6514bb383da2412d965631859a7754a4e02fecf1
Andy <bht26@cam.ac.uk>
2021-07-27 13:06:42 +0800

    clean codebase

--- src/prepare_data_transfer_learning.sh
+++ src/prepare_data_transfer_learning.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# prepare single-domain to multi-domain data
-mkdir -p data/single_to_multi/
-python utils/split_single_multi_data.py
-echo 'Done creating single-to-multi domain data, stored in data/single_to_multi/'
-exit
 
+# prepare single-domain to multi-domain data
+#mkdir -p data/single_to_multi/
+#python utils/split_single_multi_data.py
+#echo 'Done creating single-to-multi domain data, stored in data/single_to_multi/'
+# TODO: back
 
 # prepare domain transfer data
 mkdir -p data/domain_transfer
-#python3 utils/split_domain_transfer_data.py > info/split_domain_transfer.log
-#echo 'Done'
+python utils/split_domain_transfer_data.py
+echo 'Done creating domain transfer data, stored in data/domain_transfer'
